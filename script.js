1ccdc6d589ee1471e88544becfea8cdd23ab4bc7
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2024-04-18 16:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.querySelector('.First-Name');
+	let firstNameValues = firstName.value;
+	let lastName = document.querySelector('.Last-Name');
+	let lastNameValues = lastName.value;
+	alert(firstNameValues + lastNameValues);
 
 }
